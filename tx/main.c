243bdaed78bd28faa1cddc3096cb32d1b73eee50
Dmitri Vorobiev <dmitri.vorobiev@gmail.com>
2015-09-18 14:25:34 +0300

    Add scanning in the TX
    This patch adds scanning code to the TX code base.
    
    Expected result: we see many advertisement events.

--- tx/main.c
+++ tx/main.c
@@ -1,10 +1,88 @@
 #include <stdint.h>
+#include <string.h>
 
 #include "simple_uart.h"
+#include "softdevice_handler.h"
+
+#define SCAN_INTERVAL 0x00A0  /**< Determines scan interval in units of 0.625 millisecond. */
+#define SCAN_WINDOW   0x0050  /**< Determines scan window in units of 0.625 millisecond. */
+
+static ble_gap_scan_params_t m_scan_param; /**< Scan parameters requested for scanning and connection. */
+
+static void on_ble_evt(ble_evt_t *p_ble_evt)
+{
+	const ble_gap_evt_t *p_gap_evt = &p_ble_evt->evt.gap_evt;
+
+	switch (p_ble_evt->header.evt_id) {
+	case BLE_GAP_EVT_ADV_REPORT:
+		simple_uart_putstring((const uint8_t *)"Adv event\r\n");
+	break;
+	case BLE_GAP_EVT_TIMEOUT:
+		if (p_gap_evt->params.timeout.src == BLE_GAP_TIMEOUT_SRC_SCAN) {
+			simple_uart_putstring((const uint8_t *)"Scan timeout\r\n");
+		}
+		else if (p_gap_evt->params.timeout.src == BLE_GAP_TIMEOUT_SRC_CONN) {
+			simple_uart_putstring((const uint8_t *)"Conn request timeout\r\n");
+		}
+	break;
+	}
+}
+
+static void ble_evt_dispatch(ble_evt_t *p_ble_evt)
+{
+	on_ble_evt(p_ble_evt);
+}
+
+static void sys_evt_dispatch(uint32_t sys_evt)
+{
+}
+
+static void scan_start()
+{
+	/* For simplicity, we always do nonselective scan */
+
+	uint32_t err_code;
+
+	m_scan_param.active       = 0;             // Active scanning set.
+	m_scan_param.selective    = 0;             // Selective scanning not set.
+	m_scan_param.interval     = SCAN_INTERVAL; // Scan interval.
+	m_scan_param.window       = SCAN_WINDOW;   // Scan window.
+	m_scan_param.p_whitelist  = NULL;          // Provide whitelist.
+	m_scan_param.timeout      = 0x0000;        // No timeout.
+
+	err_code = sd_ble_gap_scan_start(&m_scan_param);
+	APP_ERROR_CHECK(err_code);
+}
+
+static void ble_stack_init()
+{
+	uint32_t err_code;
+
+	SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_20_PPM, NULL);
+
+	ble_enable_params_t ble_enable_params;
+
+	memset(&ble_enable_params, 0, sizeof(ble_enable_params));
+
+	ble_enable_params.gatts_enable_params.attr_tab_size   = BLE_GATTS_ATTR_TAB_SIZE_DEFAULT;
+	ble_enable_params.gatts_enable_params.service_changed = false;
+
+	err_code = sd_ble_enable(&ble_enable_params);
+	APP_ERROR_CHECK(err_code);
+
+	err_code = softdevice_ble_evt_handler_set(ble_evt_dispatch);
+	APP_ERROR_CHECK(err_code);
+
+	err_code = softdevice_sys_evt_handler_set(sys_evt_dispatch);
+	APP_ERROR_CHECK(err_code);
+}
 
 int main(void)
 {
 	simple_uart_config(12, 11, 12, 12, false);
+
+	ble_stack_init();
+	scan_start();
 
 	simple_uart_putstring((const uint8_t *)"TX goes main loop\r\n");
 
