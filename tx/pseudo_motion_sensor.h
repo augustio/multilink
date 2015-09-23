1b7372be9e3a9de3098578835c80928b603fe8f3
pkgith <petteri.koivu@metropolia.fi>
2015-09-23 09:19:30 +0300

    Update pseudo_motion_sensor.h

--- tx/pseudo_motion_sensor.h
+++ tx/pseudo_motion_sensor.h
@@ -1,10 +1,15 @@
 /*Missing lot of declarations. Should move them from pseudo_motion_sensor.c
+Probably should not use function macros
+Is this how enums work (in the .c)?
 */
 
-#ifndef MOTION_SENSOR_H
+//What is the purpose of these:
+#ifndef MOTION_SENSOR_H 
 #define MOTION_SENSOR_H
 
 #define SAVED_ACCELERATION_BUFFER_SIZE 2
+#define DELAY_WITH_STATE_CHANGE(ticks,n_s) current_motion_sensor_state=DELAY_STATE; next_state=n_s; delay_ticks=ticks
+#define VIBRATION_WITH_STATE_CHANGE(ticks,n_s,vib_mode,pause_ticks) current_motion_sensor_state=VIBRATION_STATE; next_state=n_s; delay_ticks=ticks; delay_ticks_after_vibration=pause_ticks
 
 struct accel {
 	double x[SAVED_ACCELERATION_BUFFER_SIZE];
@@ -25,5 +30,35 @@ enum Command{
 	DOWN = 0x13,
 	TOGGLE = 0x14
 };
+
+enum Motion_sensor_states
+{
+	GETTING_START_STATE,
+	RECEIVER_SELECTION,
+	RECEIVER_CONTROL,
+	DELAY_STATE,
+	VIBRATION_STATE,
+	SHUTDOWN_STATE
+};
+
+enum Vibration_modes
+{
+	VIBRATION_MODE_NORMAL,
+	VIBRATION_MODE_BUZZ_BUZZ
+};
+
+void getXYZValues();
+void getGyroValues();
+void gyroEnable();
+void gyroDisable();
+void delay();
+void vibrate();
+void endGestureControl();
+void getStartingState();
+void selectReceiver();
+void controlReceiver();
+void gestureControl();
+void timer_10ms_tick_handler();
+
 
 #endif
