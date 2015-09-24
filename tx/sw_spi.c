269d9eafe99dfaa143e80082ed70eee791cf6af0
pkgith <petteri.koivu@metropolia.fi>
2015-09-24 15:47:57 +0300

    TX: Got rid of nrf_delay_us()
    ...but replaced it with NOPs. Previous implementation was
    unnecessarily slow (frequency was ~500kHz, now ~1MHz).

--- tx/sw_spi.c
+++ tx/sw_spi.c
@@ -5,9 +5,19 @@
 #include "nordic_common.h"
 #include "sw_spi.h"
 
-/**Eleohjain LSM3DS6 connection pins */
+#define SPI_MASTER_HALF_PERIOD_DELAY __ASM ( \
+" NOP\n\t" \
+" NOP\n\t" \
+" NOP\n\t" \
+" NOP\n\t" \
+" NOP\n\t" \
+" NOP\n\t" \
+" NOP\n\t" \
+" NOP\n\t" \
+) //About 0.5 us
 
-void spi_sw_master_init() {
+void spi_sw_master_init()
+{
 	//A Slave select must be set as high before setting it as output,
 	//because during connect it to the pin it causes glitches.
 	nrf_gpio_pin_set(SPIM0_SS_PIN);
@@ -22,17 +32,18 @@ void spi_sw_master_init() {
 	nrf_gpio_pin_clear(SPIM0_MOSI_PIN);	//not required
 	//nrf_gpio_cfg_input(SPIM0_INT1_PIN, NRF_GPIO_PIN_NOPULL);
 	//*(uint8_t*)(0x0A)(FIFO_MODE_[2:0] = 001);
-
 }
 
 void spi_sw_master_send_bytes(uint16_t* tx_buffer,
-		uint8_t* rx_buffer, uint16_t buffer_len) {
-
-	uint32_t time_us = 1;
-
+		uint8_t* rx_buffer, uint16_t buffer_len)
+{
 	for (int k = 0; k < buffer_len; k++) {
 		nrf_gpio_pin_clear(SPIM0_SS_PIN);
-		nrf_delay_us(2); //necessary?
+
+		//Necessary?
+		SPI_MASTER_HALF_PERIOD_DELAY;
+		SPI_MASTER_HALF_PERIOD_DELAY;
+
 		uint8_t rx_byte = 0;
 
 		//CPOL=1,CPHA=1, msbfirst
@@ -49,24 +60,24 @@ void spi_sw_master_send_bytes(uint16_t* tx_buffer,
 				else
 					nrf_gpio_pin_clear(SPIM0_MOSI_PIN);
 
-				nrf_delay_us(time_us);
+				SPI_MASTER_HALF_PERIOD_DELAY;
 
 				nrf_gpio_pin_set(SPIM0_SCK_PIN); //rising edge
 
-				nrf_delay_us(time_us);
+				SPI_MASTER_HALF_PERIOD_DELAY;
 			}
 			rx_byte = 0;
 			if (rx_byte != 0x69) {
 				for (int i = 7; i >= 0; i--) {
 					nrf_gpio_pin_clear(SPIM0_SCK_PIN); //falling edge
-					nrf_delay_us(time_us);
+					SPI_MASTER_HALF_PERIOD_DELAY;
 					nrf_gpio_pin_set(SPIM0_SCK_PIN); //rising edge
 
 					//receive bit
 					rx_byte = rx_byte | ((uint8_t) nrf_gpio_pin_read(
 											SPIM0_MISO_PIN) << i);
 
-					nrf_delay_us(time_us);
+					SPI_MASTER_HALF_PERIOD_DELAY;
 				}
 			}
 			rx_buffer[k] = rx_byte;
@@ -87,15 +98,9 @@ void spi_sw_master_send_bytes(uint16_t* tx_buffer,
 
 				nrf_gpio_pin_set(SPIM0_SCK_PIN); //rising edge
 
-				nrf_delay_us(time_us);
+				SPI_MASTER_HALF_PERIOD_DELAY;
 			}
 			nrf_gpio_pin_set(SPIM0_SS_PIN);
 		}
 	}
-}
-
-void spi_interrupt(uint8_t* const interrupt) {
-	uint8_t inter;
-	inter = (uint8_t) nrf_gpio_pin_read(SPIM0_INT1_PIN);
-	interrupt[1] = inter;
 }
