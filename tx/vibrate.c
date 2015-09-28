47b04b224616e843dfd6a280febf499157374004
Dmitri Vorobiev <dmitri.vorobiev@gmail.com>
2015-09-28 14:07:43 +0300

    TX: reimplement vibration timers
    This is a cleaner implementation of vibration timers. No changes
    in the API.

--- tx/vibrate.c
+++ tx/vibrate.c
@@ -12,54 +12,39 @@ static app_timer_id_t m_post_vibro_timer;
 
 static bool vibrating = false;
 
-typedef struct {
-	bool *in_vibration;
-	uint32_t post_duration;
-} vibro_ctx_t;
-
 static void post_vibro_timer_handler(void *p_context)
 {
-	vibro_ctx_t *ctx = (vibro_ctx_t *)p_context;
-
+	*((bool *)p_context) = false;
 	vibrating = false;
-	*(ctx->in_vibration) = false;
 }
 
 static void vibro_timer_handler(void *p_context)
 {
-	uint32_t ticks;
-	uint32_t err_code;
-	vibro_ctx_t *ctx = (vibro_ctx_t *)p_context;
-
 	nrf_gpio_pin_clear(VIBRATOR_PIN);
-
-	ticks = APP_TIMER_TICKS(ctx->post_duration, APP_TIMER_PRESCALER);
-	err_code = app_timer_start(m_post_vibro_timer, ticks, (void *)ctx);
-	if (err_code != NRF_SUCCESS) {
-		vibrating = false;
-		*(ctx->in_vibration) = false;
-	}
 }
 
 void do_vibrate(uint32_t duration, uint32_t post_duration, bool *in_vibration)
 {
-	uint32_t ticks;
+	uint32_t ticks, post_ticks;
 	uint32_t err_code;
-	static vibro_ctx_t ctx;
 
 	if (vibrating)
 		return;
 
-	ctx.in_vibration = in_vibration;
-	ctx.post_duration = post_duration;
-
 	ticks = APP_TIMER_TICKS(duration, APP_TIMER_PRESCALER);
-	err_code = app_timer_start(m_vibro_timer, ticks, (void *)&ctx);
+	post_ticks = APP_TIMER_TICKS(post_duration, APP_TIMER_PRESCALER);
+
+	err_code = app_timer_start(m_vibro_timer, ticks, NULL);
 	if (err_code != NRF_SUCCESS) {
 		return;
 	}
 
-	/* timer ticking, set the pin */
+	err_code = app_timer_start(m_post_vibro_timer, ticks + post_ticks, (void *)in_vibration);
+	if (err_code != NRF_SUCCESS) {
+		return;
+	}
+
+	/* timers ticking, set the pin */
 	nrf_gpio_pin_set(VIBRATOR_PIN);
 	*in_vibration = true;
 	vibrating = true;
